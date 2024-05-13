b5e622066b4a211536d180a06845baaff8704a7a
Rahul S Chaubey <rahulschaubey@outlook.com>
2024-05-13 11:12:02 +0530

    Rahul: Account profile updated

--- Accounts/src/main/java/com/fitbeat/Accounts/AccountsApplication.java
+++ Accounts/src/main/java/com/fitbeat/Accounts/AccountsApplication.java
@@ -1,4 +1,4 @@
-package com.udemy.accounts;
+package com.fitbeat.Accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Accounts/src/test/java/com/fitbeat/Accounts/AccountsApplicationTests.java
+++ Accounts/src/test/java/com/fitbeat/Accounts/AccountsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.udemy.accounts;
+package com.fitbeat.Accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
