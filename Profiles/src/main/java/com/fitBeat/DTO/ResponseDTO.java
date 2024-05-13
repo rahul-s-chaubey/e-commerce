85dae52443f5e37c10782618b8d151ed86f1ff0a
Rahul S Chaubey <rahulschaubey@outlook.com>
2024-05-13 12:17:48 +0530

    Rahul: code updated in ResponseDTO

--- Profiles/src/main/java/com/fitBeat/DTO/ResponseDTO.java
+++ Profiles/src/main/java/com/fitBeat/DTO/ResponseDTO.java
@@ -9,6 +9,24 @@ public class ResponseDTO  implements Serializable {
     private Integer status;
     private String message;
     private String data;
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
 
    // private HttpStatus httpStatus;
 
